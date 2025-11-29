26783b0a7005e468beaa8ccf38fd7fda050e6d67
tsukiyomi-naoko <67056955+tsukiyomi-naoko@users.noreply.github.com>
2025-11-29 00:35:11 -0500

    minor update
    local to online

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,12 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import path from "node:path";
 
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-export default defineConfig({ plugins: [react()], server:{ host:true } })
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+});
